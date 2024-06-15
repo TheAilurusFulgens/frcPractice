848508732ca8536e720c4ffbd631d5c4369e54f5
IKKNIGHT <76535471+IKKNIGHT@users.noreply.github.com>
2024-06-14 20:54:58 -0500

    added gitpush command

--- gitpush.sh
+++ gitpush.sh
@@ -1,9 +1,30 @@
+# Check if a commit message is provided
+if [ -z "$1" ]; then
+  echo "No commit message provided."
+  echo "Usage: ./gitpush.sh \"your commit message\""
+  exit 1
+fi
+
+# Set the commit message from the first argument
+COMMIT_MSG="$1"
+
 echo "adding files to git"
 git add .
-echo "commiting files to git"
-git commit -m "commit msg"
+echo "committing files to git"
+git commit -m "$COMMIT_MSG"
 echo "pushing files to git"
 git push
 echo "--- ALL PROCESSES COMPLETED ---"
-echo "--- ISAAQ ---"
-# usage ./gitpush.sh
+echo "
+/$$$$$$                                        
+|_  $$_/                                        
+  | $$    /$$$$$$$  /$$$$$$   /$$$$$$   /$$$$$$ 
+  | $$   /$$_____/ |____  $$ |____  $$ /$$__  $$
+  | $$  |  $$$$$$   /$$$$$$$  /$$$$$$$| $$  \ $$
+  | $$   \____  $$ /$$__  $$ /$$__  $$| $$  | $$
+ /$$$$$$ /$$$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$
+|______/|_______/  \_______/ \_______/ \____  $$
+                                            | $$
+                                            | $$
+                                            |_/ 
+                                            "
