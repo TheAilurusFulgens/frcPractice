6092d2e0de4335f14c071de4d21677212777b6f8
IKKNIGHT <76535471+IKKNIGHT@users.noreply.github.com>
2024-06-14 20:14:42 -0500

    commit msg

---
+++ gitpush.sh
@@ -0,0 +1,9 @@
+echo "adding files to git"
+git add .
+echo "commiting files to git"
+git commit -m "commit msg"
+echo "pushing files to git"
+git push
+echo "--- ALL PROCESSES COMPLETED ---"
+echo "--- ISAAQ ---"
+# usage ./gitpush.sh
