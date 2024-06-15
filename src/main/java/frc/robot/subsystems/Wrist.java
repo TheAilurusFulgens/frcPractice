37456c34bd4de77996d2da8501e4d9e587bb2334
IKKNIGHT <76535471+IKKNIGHT@users.noreply.github.com>
2024-06-15 15:38:45 -0500

    Wrist with smart motion completed fr fr

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -5,21 +5,62 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Wrist extends SubsystemBase {
+  private CANSparkMax wristMotor;
+  private static final int deviceID = 1;
+  private SparkPIDController m_pidController;
+  private  RelativeEncoder m_encoder;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, maxVel, minVel, maxAcc, allowedErr;
   /** Creates a new Wrist. */
-  private CANSparkMax wristMotor = new CANSparkMax(Constants.WristConstants.WRIST_MOTOR_PORT, CANSparkMax.MotorType.kBrushless);
+  
 
   public Wrist(double position) {
+    wristMotor = new CANSparkMax(deviceID, MotorType.kBrushless);
+    wristMotor.restoreFactoryDefaults();
+    m_pidController = wristMotor.getPIDController();
+     m_encoder = wristMotor.getEncoder();
+
+     // PID coefficients
+    kP = 5e-5; 
+    kI = 1e-6;
+    kD = 0; 
+    kIz = 0; 
+    kFF = 0.000156; 
+    kMaxOutput = 1; 
+    kMinOutput = -1;
+    maxRPM = 5700;
+
+    // Smart Motion Coefficients
+    maxVel = 2000; // rpm
+    maxAcc = 1500;
+
+    // set PID coefficients
+    m_pidController.setP(kP);
+    m_pidController.setI(kI);
+    m_pidController.setD(kD);
+    m_pidController.setIZone(kIz);
+    m_pidController.setFF(kFF);
+    m_pidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    int smartMotionSlot = 0;
+    m_pidController.setSmartMotionMaxVelocity(maxVel, smartMotionSlot);
+    m_pidController.setSmartMotionMinOutputVelocity(minVel, smartMotionSlot);
+    m_pidController.setSmartMotionMaxAccel(maxAcc, smartMotionSlot);
+    m_pidController.setSmartMotionAllowedClosedLoopError(allowedErr, smartMotionSlot);
     setWristPos(position);
   }
 
   //sets wrists position 
   public void setWristPos(double pos){
-    wristMotor.getEncoder().setPosition(pos);
+ //   wristMotor.getEncoder().setPosition(pos);
+ m_pidController.setReference(pos, CANSparkMax.ControlType.kSmartMotion);
   }
 
   @Override
